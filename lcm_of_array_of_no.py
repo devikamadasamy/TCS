aee649e486f5489bb72277039b4bdea2f6415a2f
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-08-29 23:48:53 +0530

    Create lcm_of_array_of_no.py

---
+++ lcm_of_array_of_no.py
@@ -0,0 +1,19 @@
+def find_lcm(num1,num2):
+	minimum=min(num1,num2)
+	for i in range(1,minimum//2+1):
+		if num1%i==0 and num2%i==0:
+			gcd=i
+	
+	lcm=(num1*num2)//gcd
+	return lcm
+
+l = [2, 7, 3, 9, 4]
+ 
+num1 = l[0]
+num2 = l[1]
+lcm = find_lcm(num1, num2)
+ 
+for i in range(2, len(l)):
+    lcm = find_lcm(lcm, l[i])
+     
+print(lcm)
