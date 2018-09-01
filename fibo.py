c75e04a3eb6e78e5acd00988f13eda14fb75a618
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-09-01 14:56:56 +0530

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,10 @@
+n=int(input())
+fibo=[0,1]
+for i in range(2,n+1):
+	p=fibo[i-1]+fibo[i-2]
+	fibo.append(p)
+string=""
+for i in range(len(fibo)-1):
+	string=string+str(fibo[i])+" "
+string=string+str(fibo[i+1])
+print(string)
